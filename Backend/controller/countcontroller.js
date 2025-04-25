93a527437daf5fd9cb6e8a771eb7eb6cf7365ecd
VELA-ANNAMALAI <145909520+VELA-ANNAMALAI@users.noreply.github.com>
2025-04-25 23:59:56 +0530

    Update countcontroller.js

--- Backend/controller/countcontroller.js
+++ Backend/controller/countcontroller.js
@@ -1,6 +1,7 @@
 import  {Student} from '../schema/studentschema.js'; // Import the Student model
 import  Faculty  from '../schema/facultyschema.js'; // Import the Faculty model
 import Event from '../schema/EventSchema.js' // Import the Event model
+import Course from '../schema/courseschema.js';
 
 // Controller to get total counts
 export const getCounts = async (req, res) => {
@@ -8,11 +9,13 @@ export const getCounts = async (req, res) => {
     const totalStudents = await Student.countDocuments();
     const totalFaculty = await Faculty.countDocuments();
     const upcomingEvents = await Event.find({ date: { $gte: new Date() } }); // Fetch upcoming events
+    const totalCourses = await Course.countDocuments(); // Fetch total courses
 
     res.json({
       totalStudents,
       totalFaculty,
       upcomingEvents: upcomingEvents.length,
+        totalCourses,
     });
   } catch (error) {
     console.error("Error fetching counts:", error);
